110fd3b3474b910439986dc3ec8d87b1722f1d51
Kabirou ALASSANE <86885681+BlakvGhost@users.noreply.github.com>
2023-08-22 22:33:24 +0000

    Changed twitter username

--- readme.js
+++ readme.js
@@ -9,7 +9,7 @@ export default `
   <a href="https://www.youtube.com/@kabirou-dev" target="_blank" rel="noreferrer">
     <img src="https://img.shields.io/badge/YouTube-red?style=for-the-badge&logo=youtube&logoColor=white" alt="Youtube Badge"/>
   </a>
-  <a href="https://www.twitter.com/g4l1l30_77" target="_blank" rel="noreferrer">
+  <a href="https://www.twitter.com/BlakvGhost" target="_blank" rel="noreferrer">
     <img src="https://img.shields.io/badge/Twitter-blue?style=for-the-badge&logo=twitter&logoColor=white" alt="Twitter Badge"/>
   </a>
   <a href="https://dev.to/blakvghost" target="_blank" rel="noreferrer">
