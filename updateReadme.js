45e2fc9936774e8271f4e3f6bb5dc935e8803435
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-01-10 06:47:14 +0100

    Merge branch 'main' of https://github.com/BlakvGhost/BlakvGhost

--- updateReadme.js
+++ updateReadme.js
@@ -4,9 +4,9 @@ import fetchYouTubeVideos from './fetchYoutubeVideos.js';
 
 async function updateReadme() {
   const youtubeVideos = await fetchYouTubeVideos();
-  const readmeContent = readFileSync('README.md', 'utf-8');
+  const readmeContent = readFileSync('READMEx.md', 'utf-8');
   const updatedReadme = readmeContent.replace('<div id="youtube-videos"></div>', `<div id="youtube-videos">${youtubeVideos}</div>`);
-  writeFileSync('READMEx.md', updatedReadme, 'utf-8');
+  writeFileSync('README.md', updatedReadme, 'utf-8');
   console.log(youtubeVideos);
 }
 
